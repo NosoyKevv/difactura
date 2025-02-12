e7f4606636f5fc6d8b9e0c715232ad53af3b809d
Kevin Vergel <kevinvergel@wposs.com>
2025-02-12 11:16:09 -0500

    Registrando componente Items en invoice Appconfig @Bean y luego en invoice @Autowired

--- src/main/java/com/kevin/springboot/factura/springboot_difactura/AppConfig.java
+++ src/main/java/com/kevin/springboot/factura/springboot_difactura/AppConfig.java
@@ -1,9 +1,23 @@
 package com.kevin.springboot.factura.springboot_difactura;
 
+import com.kevin.springboot.factura.springboot_difactura.models.Item;
+import com.kevin.springboot.factura.springboot_difactura.models.Product;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @PropertySource("classpath:data.properties")
 public class AppConfig {
+
+    @Bean
+    List<Item> itemsInvoice() {
+        Product p1 = new Product("Camara sony", 900);
+        Product p2 = new Product("Xiaomi redmi", 1200);
+        return Arrays.asList(new Item(p1, 2), new Item(p2, 4));
+    }
 }
--- src/main/java/com/kevin/springboot/factura/springboot_difactura/models/Item.java
+++ src/main/java/com/kevin/springboot/factura/springboot_difactura/models/Item.java
@@ -5,6 +5,14 @@ public class Item {
     private Product product;
     private int quantity;
 
+    public Item() {
+    }
+
+    public Item(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
     public Product getProduct() {
         return product;
     }
