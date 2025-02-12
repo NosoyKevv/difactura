13f06c4976113cc916f8d65f4f1d989942ba0d45
Kevin Vergel <kevinvergel@wposs.com>
2025-02-12 11:44:12 -0500

    Creando controllador de invoice

--- src/main/java/com/kevin/springboot/factura/springboot_difactura/models/Item.java
+++ src/main/java/com/kevin/springboot/factura/springboot_difactura/models/Item.java
@@ -28,4 +28,8 @@ public class Item {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+
+    public int getImporte() {
+        return quantity * product.getPrice();
+    }
 }
